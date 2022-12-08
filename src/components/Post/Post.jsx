fcd7bdc7e1c11e0ddf416060825436f5b207b324
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2022-12-08 10:12:33 +0000

    feat: implement post

---
+++ src/components/Post/Post.jsx
@@ -0,0 +1,49 @@
+import { faker } from '@faker-js/faker/locale/pt_PT';
+import { PencilLine } from 'phosphor-react';
+import { Comment } from '../Comment/Comment';
+
+import styles from './Post.module.css';
+
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src={faker.image.cats(100, 100, true)} alt=""
+          />
+          <div className={styles.authorInfo}>
+            <strong>{faker.name.fullName()}</strong>
+            <span>{faker.name.jobArea()}</span>
+          </div>
+        </div>
+
+        <time title="11 de Maio as 06:12" dateTime="2022-05-11 06:12:00">
+          Públicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>{faker.lorem.sentences()}</p>
+      </div>
+
+      <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea
+          placeholder='Deixe um comentário'
+        />
+        <footer>
+          <button type='submit'>Publicar</button>
+        </footer>
+
+      </form>
+
+      <div className={styles.commentList}>
+        <Comment />
+        <Comment />
+        <Comment />
+      </div>
+    </article>
+  )
+}
