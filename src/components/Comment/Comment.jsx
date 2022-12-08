4b1522968411c2f4473cc2252a43708113720897
CaioDev <53819717+CaioGitDev@users.noreply.github.com>
2022-12-08 10:12:53 +0000

    feat: implement comment

---
+++ src/components/Comment/Comment.jsx
@@ -0,0 +1,41 @@
+import { faker } from '@faker-js/faker/locale/pt_PT';
+import { PencilLine, ThumbsUp, Trash } from 'phosphor-react';
+
+import styles from './Comment.module.css';
+
+export function Comment() {
+  return (
+    <div className={styles.comment}>
+      <img
+        className={styles.avatar}
+        src={faker.image.cats(100, 100, true)} alt=""
+      />
+
+      <div className={styles.commentBox}>
+        <div className={styles.commentContent}>
+          <header>
+            <div className={styles.authorAndTime}>
+              <strong>{faker.name.fullName()}</strong>
+              <time title="11 de Maio as 06:12" dateTime="2022-05-11 06:12:00">
+                Públicado há 1h
+              </time>
+            </div>
+            <button title="Deletar comentário">
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>{faker.lorem.sentences()}</p>
+
+          <footer>
+            <button>
+              <ThumbsUp size={24} />
+              Like <span>20</span>
+            </button>
+          </footer>
+        </div>
+
+
+      </div>
+    </div>
+  )
+}
